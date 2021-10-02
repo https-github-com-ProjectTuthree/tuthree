9670bd1a69aaf7b22de2c1e032893c4bb1f62ae3
jaeesu <wogmlsu@gmail.com>
2021-10-02 13:49:29 +0900

    일반 사용자 커뮤니티 글 목록 조회, 특정 글 조회 구현 완료

---
+++ src/test/java/project/tuthree/service/PostTestPaperServiceTest.java
@@ -0,0 +1,16 @@
+package project.tuthree.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class PostTestPaperServiceTest {
+
+}
