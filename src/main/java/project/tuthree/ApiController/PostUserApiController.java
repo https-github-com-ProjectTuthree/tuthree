c836905ff8534c20a71621d04417034c36091913
jaeesu <wogmlsu@gmail.com>
2021-10-03 16:49:24 +0900

    목록 조회 형식 수정

--- src/main/java/project/tuthree/ApiController/PostUserApiController.java
+++ src/main/java/project/tuthree/ApiController/PostUserApiController.java
@@ -3,12 +3,10 @@ package project.tuthree.ApiController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import project.tuthree.ApiController.EmbeddedResponse.ExistDataSuccessResponse;
+import project.tuthree.ApiController.EmbeddedResponse.ExistListDataSuccessResponse;
 import project.tuthree.ApiController.EmbeddedResponse.NotExistDataResultResponse;
-import project.tuthree.dto.*;
 import project.tuthree.dto.EmbeddedDTO.PostListDTO;
-import project.tuthree.dto.EmbeddedDTO.PostListData;
 import project.tuthree.repository.PostFaqRepository;
 import project.tuthree.repository.PostNoticeRepository;
 import project.tuthree.repository.PostTestPaperRepository;
@@ -16,9 +14,7 @@ import project.tuthree.service.PostFaqService;
 import project.tuthree.service.PostNoticeService;
 import project.tuthree.service.PostTestPaperService;
 import project.tuthree.service.PostTestPaperService.PaperForm;
-import project.tuthree.service.TeacherService;
 
-import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -40,23 +36,23 @@ public class PostUserApiController {
     private final PostFaqRepository postFaqRepository;
 
     // TEST PAPER ////////////////////////////////
+    /** 커뮤니티 글 목록 조회 */
     @ResponseBody
     @GetMapping("/community/{page}")
-    public ExistDataSuccessResponse CommunityList(@PathVariable("page") int page) {
+    public ExistListDataSuccessResponse CommunityList(@PathVariable("page") int page) {
         List<PostListDTO> list = postTestPaperService.communityFindByPage(page);
-        PostListData data = new PostListData(postTestPaperRepository.testpaperHasRow(), list);
         log.debug("\n---- 사용자 커뮤니티 목록 조회 [PAGE : " + page + "] ----\n");
-        return new ExistDataSuccessResponse(StatusCode.OK.getCode(),
-                page + "페이지의 글이 조회되었습니다.", data);
+        return new ExistListDataSuccessResponse(StatusCode.OK.getCode(),
+                page + "페이지의 글이 조회되었습니다.", postTestPaperRepository.testpaperHasRow(), list);
     }
 
+    /** 커뮤니티 특정 글 조회 */
     @ResponseBody
     @GetMapping("/community/id/{post_id}")
     public ExistDataSuccessResponse CommunityFind(@PathVariable("post_id") Long id) {
         PostListDTO dto = postTestPaperService.communityFindById(id);
         log.debug("\n---- 사용자 커뮤니티 글 조회 [ID : " + id + "] ----\n");
-        return new ExistDataSuccessResponse(StatusCode.OK.getCode(),
-                id + "번 게시글이 조회되었습니다.", dto);
+        return new ExistDataSuccessResponse(StatusCode.OK.getCode(), id + "번 게시글이 조회되었습니다.", dto);
     }
 
 
@@ -69,30 +65,30 @@ public class PostUserApiController {
         log.debug("\n---- 사용자 커뮤니티 글 작성 [ID : " + id + "] ----\n");
         return new NotExistDataResultResponse(StatusCode.CREATED.getCode(), id + "번 게시글이 작성되었습니다.");
     }
-    //파일 업로드 구현 후 md5 해쉬값 구현하기
 
     /** 게시글 수정 - 이미지도 수정하도록 */
     @ResponseBody
     @PutMapping("/community/id/{post_id}")
     public NotExistDataResultResponse CommunityUpdate(@PathVariable("post_id") Long id, @RequestBody PostListDTO postListDTO) {
+        /** 이전에 그 글이랑 관련된 자료는 다 지우고 새로 수정하기?? 흠... */
         Long updatedId = postTestPaperService.updateCommunity(id, postListDTO);
         log.debug("\n---- 사용자 커뮤니티 글 수정 [ID : " + id + "] ----\n");
         return new NotExistDataResultResponse(StatusCode.CREATED.getCode(), id + "번 게시글이 수정되었습니다.");
     }
 
+    /** 게시글 삭제 - 게시글과 관련된 이미지도 삭제하도록 */
 
     // FAQ ////////////////////////////////
+    /** FAQ 페이지 목록 조회 */
     @ResponseBody
     @GetMapping("/faq/{page}")
-    public ExistDataSuccessResponse FaqList(@PathVariable("page") int page) {
+    public ExistListDataSuccessResponse FaqList(@PathVariable("page") int page) {
         List<PostListDTO> postListDTO = postFaqService.faqFindByPage(page);
-        PostListData data = new PostListData(postFaqRepository.faqHasRow(), postListDTO);
         log.debug("\n---- 사용자 FAQ 목록 조회 [PAGE : " + page + "] ----\n");
-        return new ExistDataSuccessResponse(StatusCode.OK.getCode(),
-                page + "페이지의 FAQ가 조회되었습니다.", data);
-
+        return new ExistListDataSuccessResponse(StatusCode.OK.getCode(),
+                page + "페이지의 FAQ가 조회되었습니다.", postFaqRepository.faqHasRow(), postListDTO);
     }
-
+    /** FAQ 특정 글 조회 */
     @ResponseBody
     @GetMapping("/faq/id/{faq_id}")
     public ExistDataSuccessResponse FaqFind(@PathVariable("faq_id") Long id) {
@@ -103,16 +99,17 @@ public class PostUserApiController {
     }
 
     // NOTICE ////////////////////////////////
+    /** 공지사항 페이지 목록 조회 */
     @ResponseBody
     @GetMapping("/notice/{page}")
-    public ExistDataSuccessResponse NoticeList(@PathVariable("page") int page) {
+    public ExistListDataSuccessResponse NoticeList(@PathVariable("page") int page) {
         List<PostListDTO> postListDTO = postNoticeService.noticeByPage(page);
-        PostListData data = new PostListData(postNoticeRepository.noticeHasRow(), postListDTO);
         log.debug("\n---- 사용자 공지사항 목록 조회 [PAGE : " + page + "] ----\n");
-        return new ExistDataSuccessResponse(StatusCode.OK.getCode(),
-                page + "페이지의 공지사항이 조회되었습니다.", data);
+        return new ExistListDataSuccessResponse(StatusCode.OK.getCode(),
+                page + "페이지의 공지사항이 조회되었습니다.", postNoticeRepository.noticeHasRow(), postListDTO);
     }
 
+    /** 공지사항 특정 글 조회 */
     @ResponseBody
     @GetMapping("/notice/id/{notice_id}")
     public ExistDataSuccessResponse NoticeFind(@PathVariable("notice_id") Long id) {
@@ -120,6 +117,4 @@ public class PostUserApiController {
         log.debug("\n---- 사용자 공지사항 게시글 조회 [ID : " + id + "] ----\n");
         return new ExistDataSuccessResponse(StatusCode.OK.getCode(), id + "번 공지사항이 조회되었습니다.", postListDTO);
     }
-
-
 }
