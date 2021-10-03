3bfc70d77ba596f050d4a91780cdaeebe270a90b
jaeesu <wogmlsu@gmail.com>
2021-10-03 16:49:24 +0900

    목록 조회 형식 수정

--- src/main/java/project/tuthree/ApiController/EmbeddedResponse.java
+++ src/main/java/project/tuthree/ApiController/EmbeddedResponse.java
@@ -16,9 +16,22 @@ public class EmbeddedResponse {
 
     @Getter
     @AllArgsConstructor
+    public static class ExistListDataSuccessResponse<T>{
+        private final Boolean Success = true;
+        int StatusCode;
+        String Message;
+        Long list;
+        T data;
+    }
+
+    @Getter
+    @AllArgsConstructor
     public static class NotExistDataResultResponse{
         private final Boolean Success = true;
         int StatusCode;
         String Message;
     }
+
+
+
 }
--- src/main/java/project/tuthree/dto/EmbeddedDTO.java
+++ src/main/java/project/tuthree/dto/EmbeddedDTO.java
@@ -22,19 +22,4 @@ public class EmbeddedDTO {
         //계정 아이디 외의 다른 정보를 노출시키지 않기 위해 만든 클래스
     }
 
-    @Getter
-    @AllArgsConstructor
-    public static class FileListDto<T> {
-        T data;
-    }
-
-
-    @Getter
-    @AllArgsConstructor
-    public static class PostListData<T> {
-        Long list;
-        T data; //postlistdto
-    }
-
-
 }
