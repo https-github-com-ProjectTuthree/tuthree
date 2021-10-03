bde992d5d35daa33ed4adeb6e9a8bb083be809f2
kmj <rlaalswh531@gmail.com>
2021-10-03 23:19:10 +0900

    오류 더 나기 전에 일단 커밋(중복확인, 정보확인)

--- src/test/java/project/tuthree/domain/user/UserRepositoryTest.java
+++ src/test/java/project/tuthree/domain/user/UserRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import project.tuthree.domain.Status;
 
 import java.util.Date;
 import java.util.List;
@@ -20,11 +21,17 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    TeacherRepository teacherRepository;
+
     @After
-    public void cleanup() { userRepository.deleteAll();}
+    public void cleanup() {
+        userRepository.deleteAll();
+        teacherRepository.deleteAll();}
+
 
     @Test
-    public void parent_회원가입(){
+    public void parentregister(){
         String id="user123";
         String pwd="asdf1234";
         String name="user";
@@ -33,7 +40,7 @@ public class UserRepositoryTest {
         Sex sex=Sex.FEMALE;
         int birth=1999;
         String post="post/user";
-        Grade grade=Grade.PARENT;
+        Grade grade = Grade.PARENT;
 
 
         userRepository.save(User.builder()
@@ -46,7 +53,6 @@ public class UserRepositoryTest {
                 .birth(birth)
                 .post(post)
                 .grade(grade)
-                .create_date(new Date())
                 .build());
 
         List<User> userList = userRepository.findAll();
@@ -60,7 +66,76 @@ public class UserRepositoryTest {
         assertThat(user.getSex()).isEqualTo(sex);
         assertThat(user.getBirth()).isEqualTo(birth);
         assertThat(user.getPost()).isEqualTo(post);
-        assertThat(user.getGrade()).isEqualTo(grade);
+
+
+    }
+
+    @Test
+    public void teacherregister(){
+        String id="teacher123";
+        String pwd="asdf1234";
+        String name="teacher";
+        String email="teacher@user.com";
+        String tel="010-1234-1234";
+        Sex sex=Sex.FEMALE;
+        int birth=1999;
+        String post="post/user";
+        Grade grade=Grade.TEACHER;
+        String region="경기도 수원시";
+        Status registration = Status.OPEN;
+        String subject = "수학";
+        int cost = 20000;
+        String school = "경기대";
+        SchoolStatus status = SchoolStatus.ABSENCE_OF_SCHOOL;
+        String major = "컴공";
+        String certification = "certi";
+        String detail = "gd";
+
+
+
+        teacherRepository.save(Teacher.builder()
+                .id(id)
+                .pwd(pwd)
+                .name(name)
+                .email(email)
+                .tel(tel)
+                .sex(sex)
+                .birth(birth)
+                .post(post)
+                .grade(grade)
+                .create_date(new Date())
+                .region(region)
+                .registration(registration)
+                .subject(subject)
+                .cost(cost)
+                .school(school)
+                .status(status)
+                .major(major)
+                .certification(certification)
+                .detail(detail)
+                .build());
+
+        List<Teacher> teacherList = teacherRepository.findAll();
+
+        Teacher teacher = teacherList.get(0);
+        assertThat(teacher.getId()).isEqualTo(id);
+        assertThat(teacher.getPwd()).isEqualTo(pwd);
+        assertThat(teacher.getName()).isEqualTo(name);
+        assertThat(teacher.getEmail()).isEqualTo(email);
+        assertThat(teacher.getTel()).isEqualTo(tel);
+        assertThat(teacher.getSex()).isEqualTo(sex);
+        assertThat(teacher.getBirth()).isEqualTo(birth);
+        assertThat(teacher.getPost()).isEqualTo(post);
+        assertThat(teacher.getGrade()).isEqualTo(grade);
+        assertThat(teacher.getRegion()).isEqualTo(region);
+        assertThat(teacher.getRegistration()).isEqualTo(registration);
+        assertThat(teacher.getSubject()).isEqualTo(subject);
+        assertThat(teacher.getCost()).isEqualTo(cost);
+        assertThat(teacher.getSchool()).isEqualTo(school);
+        assertThat(teacher.getStatus()).isEqualTo(status);
+        assertThat(teacher.getMajor()).isEqualTo(major);
+        assertThat(teacher.getCertification()).isEqualTo(certification);
+        assertThat(teacher.getDetail()).isEqualTo(detail);
 
 
     }
