935b0a6defb54f5b6259dc3b719215af98a2c27c
kmj <rlaalswh531@gmail.com>
2021-10-04 22:12:48 +0900

    영속성, int오류 잡고 아이디 중복체크

--- src/main/java/project/tuthree/ApiController/UserApiController.java
+++ src/main/java/project/tuthree/ApiController/UserApiController.java
@@ -3,10 +3,16 @@ package project.tuthree.ApiController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import project.tuthree.domain.user.Grade;
+import project.tuthree.domain.user.User;
+import project.tuthree.dto.*;
 import project.tuthree.dto.user.*;
 import project.tuthree.service.UserRegisterService;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
 @Slf4j
 @RestController
@@ -26,6 +32,7 @@ public class UserApiController {
     public String ParentRegister(@RequestBody UserRegisterDTO registerDTO) {
         log.debug("\n---- 학부모 회원 가입 ----\n");
         return userRegisterService.createParent(registerDTO);
+
     }
 
     //학생 회원가입
@@ -42,25 +49,41 @@ public class UserApiController {
         return userRegisterService.createTeacher(registerDTO);
     }
 
-    //기본정보조회
-    @GetMapping("/user/mypage")
-    public UserResponseDTO findUserInfo(@PathVariable String id, Grade grade){
+/*    //기본정보조회
+    @GetMapping("/user/mypage/{id}")
+    public UserResponseDTO findUserInfo(@ModelAttribute UserResponseDTO userPage, Model model){
         return userRegisterService.findByInfo(id, grade);
-    }
+    }*/
 
     //튜터 과외정보조회
-    @GetMapping("/user/tutorclass")
+    @GetMapping("/user/tutorclass/{id}") //토큰값으로 조회하게 바꾸기
     public TeacherResponseDTO findTutorInfo(@PathVariable String id){
         return userRegisterService.findTutorId(id);
     }
 
     //튜티
-    @GetMapping("/user/tuteeclass")
+    @GetMapping("/user/tuteeclass/{id}")
     public StudentResponseDTO findTuteeInfo(@PathVariable String id){
         return userRegisterService.findTuTeeId(id);
     }
 
 
+    /*//정보 수정
+    @PutMapping("/user/tutorclass/{id}")
+    public Long update(@PathVariable String id, @RequestBody TeacherResponseDTO responseDTO){
+        return userRegisterService.update(id, responseDTO);
+    }
+    //정보 수정
+    @PutMapping("/user/tuteeclass/{id}")
+    public Long update(@PathVariable String id, @RequestBody StudentResponseDTO responseDTO){
+        return userRegisterService.update(id, responseDTO);
+    }
+    //정보 수정
+    @PutMapping("/user/mypage")
+    public Long update(@RequestBody UserResponseDTO responseDTO){
+        return userRegisterService.update(id, responseDTO);
+    }
+*/
 
 /*    //로그인
     @PostMapping("/login")
