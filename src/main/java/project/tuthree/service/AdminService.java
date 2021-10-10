54023e94a71edbfdad0277b2b9464d5d109d0968
kmj <rlaalswh531@gmail.com>
2021-10-11 06:58:54 +0900

    비밀번호 찾기, 변경

--- src/main/java/project/tuthree/service/AdminService.java
+++ src/main/java/project/tuthree/service/AdminService.java
@@ -2,16 +2,38 @@ package project.tuthree.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.tuthree.ApiController.EmbeddedResponse;
+import project.tuthree.ApiController.StatusCode;
+import project.tuthree.domain.user.*;
 import project.tuthree.dto.user.AdminDTO;
 import project.tuthree.repository.AdminRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AdminService {
 
     private final AdminRepository adminRepository;
+    private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
+    private final StudentRepository studentRepository;
 
     public String adminLogin(AdminDTO adminDTO) {
         return adminRepository.findByIdPwd(adminDTO.getId(), adminDTO.getPwd());
+    }
+
+    @Transactional
+    public void userList(String grade){
+        if (grade == "PARENT"){
+            userRepository.findAll();
+        }
+        else if(grade == "TEACHER"){
+            teacherRepository.findAll();
+        }
+        else{
+            studentRepository.findAll();
+        }
     }
 }
